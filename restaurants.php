485fee0496da2a05d0e10a0ad4251768b847e156
team10b-project <33267431+team10b-project@users.noreply.github.com>
2017-10-31 16:34:09 -0500

    Add files via upload

--- restaurants.php
+++ restaurants.php
@@ -2,13 +2,13 @@
 $restaurants = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for($i = 0; $i < count($restaurants); $i++){
 	if($i % 3 == 0){
-		echo	'<div class = "MenuImage First" style = "background-image: url(""); background-size: 100% 100%;"></div>';
+		echo	'<div class = "MenuImage First" alt = " . $restaurants[][] . " style = "background-image: url(" . $restaurants[][] . "); background-size: 100% 100%;"> . $restaurants[][] . </div>';
 	}
 	elseif($i % 3 == 1){
-		echo	'<div class = "MenuImage" style = "background-image: url(""); background-size: 100% 100%;"></div>';	
+		echo	'<div class = "MenuImage" alt = " . $restaurants[][] . " style = "background-image: url(" . $restaurants[][] . "); background-size: 100% 100%;"> . $restaurants[][] . </div>';	
 	}
 	elseif($i % 3 == 2){
-		echo	'<div class = "MenuImage Last" style = "background-image: url(""); background-size: 100% 100%;"></div>';
+		echo	'<div class = "MenuImage Last" alt = " . $restaurants[][] . " style = "background-image: url(" . $restaurants[][] . "); background-size: 100% 100%;"> . $restaurants[][] . </div>';
 	}
 }
 ?>
